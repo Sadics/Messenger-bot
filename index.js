32069bcb89c49f3885152d8aa7b25be004f9156d
Sadics <sadics@live.com>
2016-11-23 23:10:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.get('/', function (req, res) {
 
 // for facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+	if (req.query['hub.verify_token'] === '	
+1871129239784813|3k18dS7OZqpS6nFgyZVfFhUSDws') {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
